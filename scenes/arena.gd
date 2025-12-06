c28e446fb4b60bf603321c2a62d729d626492130
CarlosL <carlos.ledesma1983@gmail.com>
2025-12-06 13:36:38 +0100

    Agregar temporizador para generación de enemigos en la arena y ajustar la ruta de spawn

--- scenes/arena.gd
+++ scenes/arena.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
 const PLAYER_SCENE = preload("res://Player/Player.tscn")
-
+const ENEMY_SCENE = preload("res://Enemies/Basic/BaseEnemy.tscn"
+)
 @onready var spawner = $MultiplayerSpawner
-
+@onready var enemy = $MultiplayerSpawner_Enemies
 func _ready():
 	
 	# SOLO EL HOST genera los jugadores
@@ -24,3 +25,19 @@ func spawn_players():
 		# Al añadirlo como hijo, el MultiplayerSpawner detectará el nodo
 		# y lo replicará automáticamente en todos los clientes.
 		add_child(new_player)
+
+func spawn_enemy():
+	var new_enemy = ENEMY_SCENE.instantiate()
+	
+	# Posicionamiento básico aleatorio para que no salgan pegados
+	new_enemy.position = Vector2(400, 300) + Vector2(randf_range(-100, 100), randf_range(-100, 100))
+	
+	# Al añadirlo como hijo, el MultiplayerSpawner detectará el nodo
+	# y lo replicará automáticamente en todos los clientes.
+	add_child(new_enemy)
+
+func _on_timer_timeout():
+	# SOLO EL HOST genera los jugadores
+	if multiplayer.is_server():
+		spawn_enemy()
+	pass # Replace with function body.
